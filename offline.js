3fb573e98be0ebf031ef08df6dea6ef7b952e702
aymansobhey1 <47306443+aymansobhey1@users.noreply.github.com>
2025-07-04 07:20:35 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('offline-cache').then(function(cache) {
+      return cache.addAll([
+        '/p/offline.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match('https://blinfp.blogspot.com/p/offline.html');
+    })
+  );
+});
